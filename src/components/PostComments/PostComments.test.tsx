d225ead77720bd7d16b45a89374d64c11d5ac1f1
ALAN VAZ CARDOSO <145813724+Hallanx@users.noreply.github.com>
2024-04-12 19:17:36 -0300

    Add files via upload

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,24 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
+        render(<PostComment />);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
+    });
+
+    it('Deve adicionar dois comentários', () => {
+        render(<PostComment />);
+
+        const commentInput = screen.getByTestId('comment-textarea');
+        const commentButton = screen.getByTestId('comment-button');
+
+        fireEvent.change(commentInput, { target: { value: 'Comentário adicionado via testes' } });
+        fireEvent.click(commentButton);
+
+        fireEvent.change(commentInput, { target: { value: 'Segundo comentário adicionado via testes' } });
+        fireEvent.click(commentButton);
+
+        expect(screen.getAllByTestId('comment-element')).toHaveLength(2);
     });
 });
